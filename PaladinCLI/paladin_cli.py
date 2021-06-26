dbe0131bc03ccc7495f4b56212e0fea8c0b4011d
orenafek <oren.afek@gmail.com>
2021-06-26 20:34:44 +0300

    Add arguments to CLI

--- PaladinCLI/paladin_cli.py
+++ PaladinCLI/paladin_cli.py
@@ -2,24 +2,31 @@ import argparse
 import csv
 
 from engine.engine import PaLaDiNEngine
-from stubs.stubs import archive
-
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('file', help='Input .py file to PaLaDiNize')
-    parser.add_argument('--run', type=bool, default=True, help='Should run PaLaDiNized file')
-    parser.add_argument('--print-code', default=False, type=bool, help='Should print PaLaDiNized code to the screen')
+    parser.add_argument('input_file', type=str, help='Input .py file to PaLaDiNize')
+    run_group = parser.add_mutually_exclusive_group(required=True)
+    run_group.add_argument('--run', default=True, dest='run', action='store_true', help='Should run PaLaDiNized file')
+    run_group.add_argument('--no-run', default=False, dest='run', action='store_false',
+                           help='Should run PaLaDiNized file')
+    parser.add_argument('--print-code', default=False, dest='print_code', action='store',
+                        help='Should print PaLaDiNized code to the screen')
     parser.add_argument('--output-file', type=str, default='', help='Output file path of the PaLaDiNized code')
     parser.add_argument('--csv', default='', type=str, help='Should output archive results to a csv file')
 
-    return parser.parse_args()
+    args = parser.parse_args()
+
+    if args.csv != '' and not args.run:
+        parser.error('--csv can\'t be passed when --no-run is passed.')
+
+    return args
 
 
 def main():
     args = parse_args()
 
-    with open(args.file, 'r') as f:
+    with open(args.input_file, 'r') as f:
         source_code = f.read()
         paladinized_code = PaLaDiNEngine.transform(source_code)
 
@@ -27,14 +34,16 @@ def main():
             print(paladinized_code)
 
         if args.output_file != '':
-            with open(args.output_file) as fo:
+            with open(args.output_file, 'w+') as fo:
+                fo.write(PaLaDiNEngine.import_line('stubs.stubs'))
+                fo.writelines('\n' * 3)
                 fo.write(paladinized_code)
 
         if args.run:
-            PaLaDiNEngine.execute_with_paladin(source_code, args.file)
+            result, archive = PaLaDiNEngine.execute_with_paladin(paladinized_code, args.input_file)
 
         if args.csv != '':
-            with open(args.csv, 'wb+') as fo:
+            with open(args.csv, 'w+') as fo:
                 writer = csv.writer(fo)
                 header, rows = archive.to_table()
                 writer.writerow(header)
