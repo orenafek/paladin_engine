1aee1f7c9e3b72b3700f4c29efa2a8c1148201a5
orenafek <oren.afek@gmail.com>
2022-08-25 16:51:19 +0300

    Add NetAfter operator

--- PaladinEngine/archive/archive_evaluator/paladin_dsl_semantics.py
+++ PaladinEngine/archive/archive_evaluator/paladin_dsl_semantics.py
@@ -75,6 +75,12 @@ class SemanticsUtils(object):
     def displayable_time(t: int):
         return t if t >= 0 else EMPTY_TIME
 
+    @staticmethod
+    def create_empty_result(r: EvalResult):
+        if isinstance(r, bool) or r == {} or list(r.values())[0] is None:
+            raise TypeError('Cannot create an empty result.')
+        return {q: None for q in list(r.values())[0][0]}, []
+
 
 class Operator(ABC):
     @abstractmethod
@@ -515,6 +521,30 @@ class Meld(BiLateralOperator):
                         diff_mat[i][j] = (del_price, (i - 1, j)) if del_price < ins_price else (ins_price, (i, j - 1))
 
         return diff_mat
+
+
+class NextAfter(BiLateralOperator):
+    @property
+    def name(self) -> str:
+        return "NA"
+
+    def eval(self, a: EvalResult, b: EvalResult):
+        if isinstance(a, bool) or isinstance(b, bool):
+            raise TypeError('Cannot find NextAfter if either of the queries are bool')
+
+        replacements_times = lambda er: [t for t in er if any(rep.time == t for rep in er[t][1])]
+
+        a_times = replacements_times(a)
+        b_times = replacements_times(b)
+
+        time_ranges = zip(a_times, a_times[1::])
+        b_relevant_keys = list(
+            map(lambda time_range: next((t for t in b_times if t in range(*time_range)), None), time_ranges))
+
+        """{t:r for t, r in b.items() if t in list( map(lambda time_range: next((t for t in [t for t in b if any(
+        rep.time == t for rep in b[t][1])] if t in range(*time_range)), None), list(zip([t for t in a if any(rep.time 
+        == t for rep in a[t][1])], [t for t in a if any(rep.time == t for rep in a[t][1])][1::]))))} """
+        return {t: r if t in b_relevant_keys else SemanticsUtils.create_empty_result(b) for (t, r) in b.items()}
 
 
 UniLateralOperator.ALL = UniLateralOperator.__subclasses__()
