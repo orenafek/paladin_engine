1bbeae86e686e2a721400144df090ab3676f1310
Oren Afek <oren.afek@gmail.com>
2022-10-10 12:38:53 +0300

    Add a defaults option to cli ('-d')

--- PaladinUI/paladin_cli/paladin_cli.py
+++ PaladinUI/paladin_cli/paladin_cli.py
@@ -5,23 +5,28 @@ import traceback
 from PaladinUI.paladin_server.paladin_server import PaladinServer
 from archive.archive import Archive
 from engine.engine import PaLaDiNEngine
+from pathlib import *
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('input_file', type=str, help='Input .py file to PaLaDiNize')
-    run_group = parser.add_argument_group()
-    run_group.add_argument('--run', default=False, dest='run', action='store_true', help='Should run PaLaDiNized file')
+    mux_group = parser.add_mutually_exclusive_group()
+    mux_group.add_argument('-d', '--defaults', action='store_true',
+                           help='Use defaults for output and csv files')
+    details_group = mux_group.add_argument_group()
+    run_group = details_group.add_argument_group()
+    run_group.add_argument('--run', default=True, dest='run', action='store_true', help='Should run PaLaDiNized file')
     run_group.add_argument('-to', '--timeout', type=int, nargs='?', action='store', default=-1,
                            help='Add timeout to the program in seconds')
-    parser.add_argument('--print-code', default=False, dest='print_code', action='store',
-                        help='Should print PaLaDiNized code to the screen')
-    parser.add_argument('--output-file', type=str, default='', help='Output file path of the PaLaDiNized code')
-    parser.add_argument('--csv', default='', type=str, help='Should output archive results to a csv file')
-    parser.add_argument('--run-debug-server', default=False, dest='run_debug_server', action='store',
-                        help='Should run PaLaDiN-Debug server')
-    parser.add_argument('--port', default=9999, type=int,
-                        help='The port no of the server in which the dynamic graph runs on.')
+    details_group.add_argument('--print-code', default=False, dest='print_code', action='store',
+                               help='Should print PaLaDiNized code to the screen')
+    details_group.add_argument('--output-file', type=str, default='', help='Output file path of the PaLaDiNized code')
+    details_group.add_argument('--csv', default='', type=str, help='Should output archive results to a csv file')
+    details_group.add_argument('--run-debug-server', default=True, dest='run_debug_server', action='store',
+                               help='Should run PaLaDiN-Debug server')
+    details_group.add_argument('-p', '--port', default=9999, type=int,
+                               help='The port no of the server in which the dynamic graph runs on')
     args = parser.parse_args()
 
     if args.csv != '' and not args.run:
@@ -31,10 +36,25 @@ def parse_args():
 
 
 # noinspection PyBroadException
+def fill_defaults(args):
+    input_file = Path(args.input_file).absolute()
+    output_file = input_file.parent / Path(input_file.stem + '_output.py')
+    csv_file = input_file.parent / input_file.with_suffix('.csv')
+
+    args.output_file = output_file
+    args.csv_file = csv_file
+
+    return args
+
+
 def main():
     args = parse_args()
     archive = None
     thrown_exception = None
+
+    if args.defaults:
+        args = fill_defaults(args)
+
     with open(args.input_file, 'r') as f:
         source_code = f.read()
         paladinized_code = PaLaDiNEngine.transform(source_code)
