d2a2246c75d80f079514af34d6869df5c0f8e19d
avital4 <62469581+avital4@users.noreply.github.com>
2022-12-24 23:47:40 +0200

    [diff-tool]: add a new example - "majority_element"

---
+++ PaladinEngine/tests/test_resources/examples/majority_element/majority_element_1.py
@@ -0,0 +1,16 @@
+def majority_element_1(nums):
+    m = {}
+    for n in nums:
+        m[n] = m.get(n, 0) + 1
+        if m[n] > len(nums) // 2:
+            return n
+
+
+def main():
+    numbers = [3, 2, 3, 2, 2, 2, 4]
+    result1 = majority_element_1(numbers)
+    print(result1)
+
+
+if __name__ == "__main__":
+    main()
---
+++ PaladinEngine/tests/test_resources/examples/majority_element/majority_element_2.py
@@ -0,0 +1,20 @@
+def majority_element_2(nums):
+    candidate, count = nums[0], 0
+    for num in nums:
+        if num == candidate:
+            count += 1
+        elif count == 0:
+            candidate, count = num, 1
+        else:
+            count -= 1
+    return candidate
+
+
+def main():
+    numbers = [3, 2, 3, 2, 2, 2, 4]
+    result2 = majority_element_2(numbers)
+    print(result2)
+
+
+if __name__ == "__main__":
+    main()
