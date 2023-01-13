9772d7961afcc7ec83a5f6ac4263d71069e68fbb
avital4 <62469581+avital4@users.noreply.github.com>
2023-01-13 13:30:35 +0200

    [diff-tool]: continue working on merge_tables

--- DiffTool/paladin_diff_tool.py
+++ DiffTool/paladin_diff_tool.py
@@ -98,14 +98,52 @@ def _are_matching_rows(row1, row2, merge_condition_1, merge_condition_2):
     return sub_row1.equals(sub_row2)
 
 
-def _print_matching_rows(row1, row2):
-    merge_row = pd.concat([row1, row2])
-    print(merge_row)
+def _search_for_next_match(table_a, index_a, table_a_unmatched_rows, table_b_unmatched_rows, merge_condition_a, merge_condition_b):
+    index_a += 1
+
+    if index_a >= len(table_a):
+        return False, table_a_unmatched_rows, table_b_unmatched_rows, index_a
+
+    row_a = table_a.iloc[[index_a]]
+    table_a_unmatched_rows.append((row_a, index_a))
+    is_match_found = False
+    for i, (row2_unmatched, index2_unmatched) in enumerate(table_b_unmatched_rows):
+        if _are_matching_rows(row_a, row2_unmatched, merge_condition_a, merge_condition_b):
+            is_match_found = True
+            table_b_unmatched_rows = table_b_unmatched_rows[:i + 1]
+            break
+    return is_match_found, table_a_unmatched_rows, table_b_unmatched_rows, index_a
 
 
-def _print_unmatching_rows(rows1, rows2):
-    pass #TODO: IMPLEMENT
+def _print_matching_rows(result, row1, row2):
+    row1.reset_index(drop=True)
+    row2.reset_index(drop=True)
+    merged_row = pd.concat([row1, row2], axis=1)
+    result = pd.concat([result, merged_row], ignore_index=True)
+    # print(result)
+    return result
 
+
+def _print_unmatching_rows(result, rows1, rows2, table1_columns, table2_columns):
+    df1 = pd.DataFrame(columns=table1_columns)
+    print(len(rows1), len(rows2))
+    if not rows1.empty:
+        print(type(rows1), rows1)
+        for row in rows1:
+            print(row, type(row), row.empty)
+            df1 = pd.concat([df1, row])
+        df1.reset_index(drop=True)
+    df2 = pd.DataFrame(columns=table2_columns)
+    if not len(rows2):
+        for row in rows2:
+            df2 = pd.concat([df2, row])
+        df2.reset_index(drop=True)
+
+    merged_block = pd.concat([df1, df2], axis=1)
+    print(merged_block)
+    print(result)
+    result = pd.concat([result, merged_block], ignore_index=True)
+    return result
 
 # ---------------------------------------------------------------------------------------------#
 # ------------------------- INTERACTIVE FUNCTIONS ---------------------------------------------#
@@ -119,6 +157,11 @@ def get_data(csv_file_path, query, suffix, start_time=0, end_time=500):
 
 
 def merge_tables(table1, table2, merge_condition_1, merge_condition_2, suffix1, suffix2):
+    table1_columns = table1.columns.values.tolist()
+    table2_columns = table2.columns.values.tolist()
+    all_columns = table1_columns + table2_columns
+    result = pd.DataFrame(columns=all_columns)
+
     merge_condition_1 = [item + suffix1 for item in merge_condition_1]
     merge_condition_2 = [item + suffix2 for item in merge_condition_2]
 
@@ -128,39 +171,31 @@ def merge_tables(table1, table2, merge_condition_1, merge_condition_2, suffix1, 
         row1 = table1.iloc[[index1]]
         row2 = table2.iloc[[index2]]
         if _are_matching_rows(row1, row2, merge_condition_1, merge_condition_2):
-            _print_matching_rows(row1, row2)
+            result = _print_matching_rows(result, row1, row2)
             index1 += 1
             index2 += 1
         else:
             table1_unmatched_rows = [(row1, index1)]
             table2_unmatched_rows = [(row2, index2)]
             is_match_found = False
-            while not is_match_found:
-                index1 += 1
-                row1 = table1.iloc[[index1]]
-                for row2_unmatched, index2_unmatched in table2_unmatched_rows:
-                    if _are_matching_rows(row1, row2_unmatched, merge_condition_1, merge_condition_2):
-                        is_match_found = True
-                        index1 += 1
-                        index2 = index2_unmatched + 1
-                        _print_unmatching_rows(table1_unmatched_rows, table2_unmatched_rows) #TODO: table2 from index 0 to index2_unmatched; pass only rows without indices
-                        break
+            while not is_match_found and (index1 < len(table1) or index2 < len(table2)):
+                is_match_found, table1_unmatched_rows, table2_unmatched_rows, index1 = _search_for_next_match(
+                    table1, index1, table1_unmatched_rows, table2_unmatched_rows, merge_condition_1, merge_condition_2
+                )
                 if is_match_found:
                     break
-                table1_unmatched_rows.append((row1, index1))
+                is_match_found, table2_unmatched_rows, table1_unmatched_rows, index2 = _search_for_next_match(
+                    table2, index2, table2_unmatched_rows, table1_unmatched_rows, merge_condition_2, merge_condition_1
+                )
+            index1 = table1_unmatched_rows[-1][1] + 1
+            index2 = table2_unmatched_rows[-1][1] + 1
+            # Print the unmatched block
+            table1_unmatched_rows_data = [item[0] for item in table1_unmatched_rows]
+            table2_unmatched_rows_data = [item[0] for item in table2_unmatched_rows]
+            result = _print_unmatching_rows(result, table1_unmatched_rows_data, table2_unmatched_rows_data, table1_columns, table2_columns)
+    # Print remaining rows
+    # print(table1.iloc[index1:])
+    # print(table2.iloc[index2:])
+    result = _print_unmatching_rows(result, table1.iloc[index1:], table2.iloc[index2:], table1_columns, table2_columns)
 
-                index2 += 1
-                row2 = table2.iloc[[index2]]
-                for row1_unmatched, index1_unmatched in table1_unmatched_rows:
-                    if _are_matching_rows(row2, row1_unmatched, merge_condition_2, merge_condition_1):
-                        is_match_found = True
-                        index2 += 1
-                        index1 = index1_unmatched + 1
-                        _print_unmatching_rows(table2_unmatched_rows, table1_unmatched_rows) #TODO: table1 from index 0 to index1_unmatched; pass only rows without indices
-
-                if is_match_found:
-                    break
-                table2_unmatched_rows.append((row2, index2))
-
-            #TODO: inside for loop whem match is found - update the unmatched_rows lists to contain only relevant items,
-            #TODO: and then here take the last index +1 and update index1, index2 and also print from here
+    print(result)
