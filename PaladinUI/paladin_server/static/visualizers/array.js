ab8112c8a6f26f81c716bf2fdcd4db4badea4b63
Oren Afek <oren.afek@gmail.com>
2024-03-26 16:15:46 +0200

    Add a graph visualizer

---
+++ PaladinUI/paladin_server/static/visualizers/array.js
@@ -0,0 +1,48 @@
+class ArrayDisplayer {
+
+    matches(data) {
+        return data?.hasOwnProperty('arr');
+    }
+
+    format() {
+        return {
+            type: "text/html",
+            content:
+                `
+                <table style="border-collapse: collapse;">
+                    <tr style="color: yellow">
+                        <template v-for="(item, index) in data.arr">
+                            <td v-if="(index >= (data.low ?? undefined)) && index <= (data.high ?? undefined)" style="border: 1px solid white; background-color: lightpink;">
+                                {{item}}
+                            </td>
+                            <td v-else style="border: 1px solid white; border-collapse: collapse;">
+                                {{item}}
+                            </td>
+                        </template>
+                    </tr>
+                    <tr style="color: red">
+                        <td v-for="(_, index) in data.arr">
+                            <span v-if="[data.p, (data.i > 0 ? data.i : undefined), data.j].includes(index)">
+                                <span>&#8593;</span>
+                            </span>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td v-for="(_, index) in data.arr">
+                            <span v-if="index === (data.p ?? undefined)">
+                                p
+                            </span>
+                            <span v-if="index === (data.i ?? undefined) && data.i > 0">
+                                i
+                            </span>
+                            <span v-if="index === (data.j ?? undefined)">
+                                j
+                            </span>
+                        </td>
+                    </tr>
+                </table>
+                `
+
+        }
+    }
+}
