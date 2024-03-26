77514bbc4ae65c148c9ecad2051d04a83a3ed255
Oren Afek <oren.afek@gmail.com>
2024-03-26 16:12:17 +0200

    Add the ability to add dynamic html-elements

--- PaladinUI/paladin_server/src/components/runtime-component.vue
+++ PaladinUI/paladin_server/src/components/runtime-component.vue
@@ -2,6 +2,9 @@
     <div v-if="type === 'text/html'">
         <v-runtime-template :template="content"></v-runtime-template>
     </div>
+    <div v-else-if="type === 'htmlElement'">
+        <div ref="htmlElementContainer" />
+    </div>
     <div v-else>
         {{ data }}
     </div>
@@ -16,6 +19,15 @@ class RuntimeComponent extends Vue {
     @Prop type: string
     @Prop content: string
     @Prop data: any
+
+    $refs: {htmlElementContainer: HTMLDivElement}
+
+    mounted() {
+        if (this.$refs.htmlElementContainer) {
+            this.$refs.htmlElementContainer.append(this.content);
+        }
+
+    }
 }
 
 export default toNative(RuntimeComponent);
