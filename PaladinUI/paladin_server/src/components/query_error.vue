772d97679c17474898a5ac24a73354c0b7a9bbdf
Oren Afek <oren.afek@gmail.com>
2024-04-25 17:03:09 +0300

    Add a query error comp. and send error from server

---
+++ PaladinUI/paladin_server/src/components/query_error.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="query-error"> {{ error }} </div>
+</template>
+
+<script lang="ts">
+import {Component, Prop, toNative, Vue} from "vue-facing-decorator";
+
+@Component
+class QueryError extends Vue {
+    @Prop error: string
+}
+
+export default toNative(QueryError);
+
+</script>
+
+<style lang="scss" scoped>
+.query-error {
+    color: red;
+}
+</style>
